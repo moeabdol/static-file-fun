cb7da09313e7c41c362ad3a920dfd2abc31bbc28
moeabdol <mohd.a.saed@gmail.com>
2017-08-14 23:22:40 +0300

    Implement basic app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path    = require("path");
+const fs      = require("fs");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Request IP: " + req.url);
+  console.log("Request date: " + new Date());
+  next();
+});
+
+app.listen(3000, () => {
+  console.log("App started on port 3000");
+});
